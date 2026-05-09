3df33500b0eb1a4628ca461cdc042205fe4e5c97
fricosystem <fricoalimentossystem@gmail.com>
2026-05-08 23:06:44 -0300

    Create screen Autenticacao

---
+++ src/Autenticacao.scm
@@ -0,0 +1,11 @@
+{
+  "Properties": {
+    "$Type": "Form",
+    "$Name": "Autenticacao",
+    "Title": "Autenticacao",
+    "BackgroundColor": "&HFFFFFFFF",
+    "AlignHorizontal": "1",
+    "AlignVertical": "1",
+    "$Components": []
+  }
+}
