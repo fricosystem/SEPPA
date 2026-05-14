0b0e282c702392bd68b75ba83bd476ad092a6ec3
fricosystem <fricoalimentossystem@gmail.com>
2026-05-14 18:36:24 -0300

    Auto-sync design/code

--- src/io/kodular/bruno_bm3051/SEPPA_OM01/Screen1.scm
+++ src/io/kodular/bruno_bm3051/SEPPA_OM01/Screen1.scm
@@ -1,4 +1,50 @@
 #|
 $JSON
-{"authURL":["creator.kodular.io"],"YaVersion":"245","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"46","AlignHorizontal":"3","AlignVertical":"2","AppId":"768847495005243321","AppName":"Mercado Pago Kodular","BackgroundColor":"&HFF000000","BackgroundImage":"Nenhum","HighQuality":"True","OpenScreenAnimation":"fade","PackageName":"com.pag.deivid","ReceiveSharedText":"none","ScreenOrientation":"portrait","ShowListsAsJson":"True","ShowOptionsMenu":"False","ShowStatusBar":"False","Title":"Screen1","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"Imagem1","$Type":"Image","$Version":"7","Height":"-2","Width":"-2","Picture":"Sistema_de_estoque.png","Uuid":"1398737707"},{"$Name":"Temporizador1","$Type":"Clock","$Version":"4","TimerAlwaysFires":"False","TimerInterval":"5000","Uuid":"1957495746"}]}}
-|#
+{
+  "authURL": [
+    "creator.kodular.io"
+  ],
+  "YaVersion": "245",
+  "Source": "Form",
+  "Properties": {
+    "$Name": "Screen1",
+    "$Type": "Form",
+    "$Version": "46",
+    "AlignHorizontal": "3",
+    "AlignVertical": "2",
+    "AppId": "768847495005243321",
+    "AppName": "Mercado Pago Kodular",
+    "BackgroundColor": "&HFF000000",
+    "BackgroundImage": "Nenhum",
+    "HighQuality": "True",
+    "OpenScreenAnimation": "fade",
+    "PackageName": "com.pag.deivid",
+    "ReceiveSharedText": "none",
+    "ScreenOrientation": "portrait",
+    "ShowListsAsJson": "True",
+    "ShowOptionsMenu": "False",
+    "ShowStatusBar": "False",
+    "Title": "Screen1",
+    "TitleVisible": "False",
+    "Uuid": "0",
+    "$Components": [
+      {
+        "$Name": "Imagem1",
+        "$Type": "Image",
+        "$Version": "7",
+        "Height": "-2",
+        "Width": "-2",
+        "Picture": "Sistema_de_estoque.png",
+        "Uuid": "1398737707"
+      },
+      {
+        "$Name": "Temporizador1",
+        "$Type": "Clock",
+        "$Version": "4",
+        "TimerAlwaysFires": "False",
+        "TimerInterval": "5000",
+        "Uuid": "1957495746"
+      }
+    ]
+  }
+}
