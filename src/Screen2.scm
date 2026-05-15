c1552e97bbfe81ce41ab0902d26cdcacd7e5de1b
fricosystem <fricoalimentossystem@gmail.com>
2026-05-15 00:12:57 -0300

    Auto-sync design/code

--- src/Screen2.scm
+++ src/Screen2.scm
@@ -10,39 +10,135 @@
     "Uuid": "0",
     "$Components": [
       {
-        "$Name": "ScannerLayout",
         "$Type": "VerticalArrangement",
-        "$Version": "1",
-        "Name": "ScannerLayout",
-        "AlignVertical": "Center",
-        "Height": "-2",
-        "AlignHorizontal": "Center",
+        "$Name": "VerticalArrangement1",
+        "BackgroundColor": "&HFFFFFFFF",
         "Width": "-2",
+        "Height": "-2",
+        "AlignHorizontal": 3,
+        "AlignVertical": 2,
         "$Components": [
           {
-            "$Name": "Label1",
             "$Type": "Label",
-            "$Version": "1",
-            "Name": "ScanStatusLabel",
-            "Text": "APEX",
-            "FontBold": "True",
-            "FontSize": "18",
-            "HTMLFormat": "False"
+            "$Name": "Label1",
+            "Text": "Padaria Delícia",
+            "FontSize": 24,
+            "FontBold": true,
+            "TextColor": "&HFF4F46E5"
           },
           {
-            "$Name": "Button1",
-            "$Type": "Button",
-            "$Version": "1",
-            "Text": "Escanear",
-            "Name": "ScanButton"
+            "$Type": "HorizontalArrangement",
+            "$Name": "HorizontalArrangement1",
+            "Width": "-2",
+            "Height": "-1",
+            "$Components": [
+              {
+                "$Type": "Button",
+                "$Name": "Button1",
+                "Text": "Pães",
+                "FontSize": 18,
+                "FontBold": true,
+                "TextColor": "&HFFFFFFFF",
+                "BackgroundColor": "&HFF4F46E5",
+                "Shape": 1,
+                "Width": "-1",
+                "Height": "-1"
+              },
+              {
+                "$Type": "Button",
+                "$Name": "Button2",
+                "Text": "Bolos",
+                "FontSize": 18,
+                "FontBold": true,
+                "TextColor": "&HFFFFFFFF",
+                "BackgroundColor": "&HFF4F46E5",
+                "Shape": 1,
+                "Width": "-1",
+                "Height": "-1"
+              },
+              {
+                "$Type": "Button",
+                "$Name": "Button3",
+                "Text": "Tortas",
+                "FontSize": 18,
+                "FontBold": true,
+                "TextColor": "&HFFFFFFFF",
+                "BackgroundColor": "&HFF4F46E5",
+                "Shape": 1,
+                "Width": "-1",
+                "Height": "-1"
+              },
+              {
+                "$Type": "Button",
+                "$Name": "Button4",
+                "Text": "Sanduíches",
+                "FontSize": 18,
+                "FontBold": true,
+                "TextColor": "&HFFFFFFFF",
+                "BackgroundColor": "&HFF4F46E5",
+                "Shape": 1,
+                "Width": "-1",
+                "Height": "-1"
+              },
+              {
+                "$Type": "Button",
+                "$Name": "Button5",
+                "Text": "Bebidas",
+                "FontSize": 18,
+                "FontBold": true,
+                "TextColor": "&HFFFFFFFF",
+                "BackgroundColor": "&HFF4F46E5",
+                "Shape": 1,
+                "Width": "-1",
+                "Height": "-1"
+              }
+            ]
+          },
+          {
+            "$Type": "Label",
+            "$Name": "Label2",
+            "Text": "Produtos em Destaque",
+            "FontSize": 20,
+            "FontBold": true,
+            "TextColor": "&HFF4F46E5"
+          },
+          {
+            "$Type": "HorizontalArrangement",
+            "$Name": "HorizontalArrangement2",
+            "Width": "-2",
+            "Height": "-1",
+            "$Components": [
+              {
+                "$Type": "Image",
+                "$Name": "Image1",
+                "Picture": "pao.jpg",
+                "Width": "-1",
+                "Height": "-1"
+              },
+              {
+                "$Type": "Image",
+                "$Name": "Image2",
+                "Picture": "bolo.jpg",
+                "Width": "-1",
+                "Height": "-1"
+              },
+              {
+                "$Type": "Image",
+                "$Name": "Image3",
+                "Picture": "torta.jpg",
+                "Width": "-1",
+                "Height": "-1"
+              }
+            ]
+          },
+          {
+            "$Type": "Label",
+            "$Name": "Label3",
+            "Text": "Contato: (11) 1234-5678 | redes sociais: @padariadelicia",
+            "FontSize": 16,
+            "TextColor": "&HFF4F46E5"
           }
         ]
-      },
-      {
-        "$Type": "TextBox",
-        "$Name": "TextBox1",
-        "Text": "OlÃ¡aa",
-        "Hint": "Oiiiii"
       }
     ]
   }
