8f3bfa7629a046376944d1a8dd5e187d321977a1
fricosystem <fricoalimentossystem@gmail.com>
2026-05-16 14:32:52 -0300

    Create screen Formulrio

---
+++ src/Formulrio.scm
@@ -0,0 +1,11 @@
+{
+  "Properties": {
+    "$Type": "Form",
+    "$Name": "Formulrio",
+    "Title": "Formulrio",
+    "BackgroundColor": "&HFFFFFFFF",
+    "AlignHorizontal": "1",
+    "AlignVertical": "1",
+    "$Components": []
+  }
+}
