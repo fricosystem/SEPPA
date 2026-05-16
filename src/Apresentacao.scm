55357016f491c74ad9ab8b30a400e29176e39abb
fricosystem <fricoalimentossystem@gmail.com>
2026-05-16 18:50:34 -0300

    Auto-sync design/code

--- src/Apresentacao.scm
+++ src/Apresentacao.scm
@@ -7,57 +7,19 @@
     "$Components": [
       {
         "$Type": "VerticalArrangement",
-        "$Name": "ConteinerPrincipal",
-        "AlignHorizontal": 3,
-        "AlignVertical": 2,
+        "$Name": "VerticalArrangement1",
         "Width": -2,
         "Height": -2,
-        "$Components": [
-          {
-            "$Type": "CardView",
-            "$Name": "CartaoPrincipal",
-            "CornerRadius": 20,
-            "Elevation": 10,
-            "Width": -2,
-            "Height": -1,
-            "BackgroundColor": "&HFFFFFFFF",
-            "$Components": [
-              {
-                "$Type": "VerticalArrangement",
-                "$Name": "Conteudo",
-                "Width": -2,
-                "Height": -1,
-                "Padding": 20,
-                "$Components": [
-                  {
-                    "$Type": "Label",
-                    "$Name": "Titulo",
-                    "Text": "Bem-vindo!",
-                    "FontBold": true,
-                    "FontSize": 22,
-                    "TextColor": "&HFF121212"
-                  },
-                  {
-                    "$Type": "Label",
-                    "$Name": "Subtitulo",
-                    "Text": "Este é um exemplo de tela de apresentação",
-                    "FontSize": 14,
-                    "TextColor": "&HFF6B7280"
-                  },
-                  {
-                    "$Type": "Button",
-                    "$Name": "BotaoEntrar",
-                    "Text": "Entrar",
-                    "FontBold": true,
-                    "FontSize": 15,
-                    "BackgroundColor": "&HFF4F46E5",
-                    "TextColor": "&HFFFFFFFF"
-                  }
-                ]
-              }
-            ]
-          }
-        ]
+        "AlignHorizontal": 3,
+        "AlignVertical": 2
+      },
+      {
+        "$Type": "CardView",
+        "$Name": "CardView1",
+        "CornerRadius": 20,
+        "Elevation": 5,
+        "Width": -2,
+        "BackgroundColor": "&HFFFFFFFF"
       }
     ]
   }
