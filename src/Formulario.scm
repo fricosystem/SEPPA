35bae5fd8a99da0dcbb59654b1cf89b0c7182046
fricosystem <fricoalimentossystem@gmail.com>
2026-05-16 16:31:26 -0300

    Auto-sync design/code

--- src/Formulario.scm
+++ src/Formulario.scm
@@ -6,14 +6,6 @@
     "BackgroundColor": "&HFFFFFFFF",
     "AlignHorizontal": "1",
     "AlignVertical": "1",
-    "$Components": [
-      {
-        "$Type": "Button",
-        "$Name": "Button1",
-        "Text": "Olá",
-        "BackgroundColor": "&HFF000000",
-        "TextColor": "&HFFFFFFFF"
-      }
-    ]
+    "$Components": []
   }
 }
