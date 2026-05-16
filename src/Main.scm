26c9cfb35984ae0908e83b58f26cfb4097729e93
fricosystem <fricoalimentossystem@gmail.com>
2026-05-16 18:14:31 -0300

    Create screen Main

---
+++ src/Main.scm
@@ -0,0 +1,11 @@
+{
+  "Properties": {
+    "$Type": "Form",
+    "$Name": "Main",
+    "Title": "Main",
+    "BackgroundColor": "&HFFFFFFFF",
+    "AlignHorizontal": "1",
+    "AlignVertical": "1",
+    "$Components": []
+  }
+}
